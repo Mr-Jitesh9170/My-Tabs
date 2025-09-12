583b884ced6996eb7489baef76f40aea10fbf139
Mr-Jitesh9170 <mr.jiteshpandey9170@gmail.com>
2025-09-12 14:21:00 +0530

    worked on creating tabs

---
+++ data.js
@@ -0,0 +1,13 @@
+export let mytabs = [
+    {
+        name: "GFG",
+        icons: "",
+        links: ""
+    },
+    {
+        name: "Leetcode",
+        icons: "",
+        links: ""
+    }, 
+]
+
--- mytabs.js
+++ mytabs.js
@@ -1,11 +1,39 @@
-document.getElementById("changeColor").addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+import { mytabs } from "./data.js";
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: () => {
-      document.body.style.backgroundColor =
-        document.body.style.backgroundColor === "lightblue" ? "white" : "lightblue";
-    }
+const tablists = document.getElementById("tablists");
+const addBtn = document.getElementById("addNewTab")
+
+mytabs.forEach(tab => {
+  const tabDetails = document.createElement("a");
+  tabDetails.className = "tabDetails";
+  tabDetails.href = "https://github.com/Mr-Jitesh9170/My-Tabs"
+
+  const tabName = document.createElement("div");
+  tabName.className = "tabName";
+  tabName.innerText = tab.name;
+
+  const tabImg = document.createElement("div");
+  tabImg.className = "tabImg";
+  const imgTag = document.createElement("img");
+  imgTag.src = tab.icons || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwJTFfoVZ4-V-Zt8oj5O3AnpHaYlrOEpVgRg&s";
+  tabImg.appendChild(imgTag);
+
+  tabDetails.appendChild(tabImg);
+  tabDetails.appendChild(tabName);
+
+  tablists.appendChild(tabDetails);
+});
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  Array.from(inputFields).forEach((data) => {
+    const newTab = {
+      name: data.value,
+      icons: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwJTFfoVZ4-V-Zt8oj5O3AnpHaYlrOEpVgRg&s",
+      links: "https://github.com/Mr-Jitesh9170/My-Tabs"
+    };
+    mytabs.push(newTab);
+    createTab(newTab); 
+    data.value = ""; 
   });
 });
