6633cee15d55e22285651f76e17287822e3b4521
Mr-Jitesh9170 <mr.jiteshpandey9170@gmail.com>
2025-09-14 23:00:00 +0530

    working on creating new tabs

--- mytabs.js
+++ mytabs.js
@@ -1,12 +1,13 @@
 import { mytabs } from "./data.js";
 
 const tablists = document.getElementById("tablists");
-const addBtn = document.getElementById("addNewTab")
+const addBtn = document.getElementById("addNewTab");
 
-mytabs.forEach(tab => {
+// ✅ reusable function to render a tab
+function createTab(tab) {
   const tabDetails = document.createElement("a");
   tabDetails.className = "tabDetails";
-  tabDetails.href = "https://github.com/Mr-Jitesh9170/My-Tabs"
+  tabDetails.href = tab.links || "https://github.com/Mr-Jitesh9170/My-Tabs";
 
   const tabName = document.createElement("div");
   tabName.className = "tabName";
@@ -15,25 +16,37 @@ mytabs.forEach(tab => {
   const tabImg = document.createElement("div");
   tabImg.className = "tabImg";
   const imgTag = document.createElement("img");
-  imgTag.src = tab.icons || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwJTFfoVZ4-V-Zt8oj5O3AnpHaYlrOEpVgRg&s";
+  imgTag.src =
+    tab.icons ||
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwJTFfoVZ4-V-Zt8oj5O3AnpHaYlrOEpVgRg&s";
   tabImg.appendChild(imgTag);
 
   tabDetails.appendChild(tabImg);
   tabDetails.appendChild(tabName);
 
   tablists.appendChild(tabDetails);
+}
+
+// render initial tabs
+mytabs.forEach((tab) => {
+  createTab(tab);
 });
 
+// add new tab on button click
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  console.log("cdfdj");
+
+  let inputFields = document.getElementsByClassName("input");
   Array.from(inputFields).forEach((data) => {
     const newTab = {
       name: data.value,
-      icons: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwJTFfoVZ4-V-Zt8oj5O3AnpHaYlrOEpVgRg&s",
-      links: "https://github.com/Mr-Jitesh9170/My-Tabs"
+      icons:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwJTFfoVZ4-V-Zt8oj5O3AnpHaYlrOEpVgRg&s",
+      links: "https://github.com/Mr-Jitesh9170/My-Tabs",
     };
     mytabs.push(newTab);
-    createTab(newTab); 
-    data.value = ""; 
+    createTab(newTab); // ✅ now this works
+    data.value = "";
   });
 });
